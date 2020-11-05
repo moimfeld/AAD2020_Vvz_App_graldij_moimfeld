b66283488818a95d6ccb1359c6bf0d8f6e62d276
moimfeld <moritz.imfeld@gmail.com>
2020-11-05 09:09:23 +0100

    Version 0.1 (05.11.2020)

--- app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Timetable.java
+++ app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Timetable.java
@@ -10,39 +10,37 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class Timetable extends AppCompatActivity {
+
+
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_timetable);
 
 
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.action_MainActivity);
+        bottomNavigationView.setSelectedItemId(R.id.action_Timetable);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_Timetable:
-                        //Toast.makeText(MainActivity.this, "Recents", Toast.LENGTH_SHORT).show();
-                        Intent Timetable = new Intent(MainActivity.this,Timetable.class);
-                        startActivity(Timetable);
-
-                        //This line makes a smooth transition and is used for all transitions in all the activities
-                        overridePendingTransition(0, 0);
-
+                        //Toast.makeText(Timetable.this, "Recents", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_MainActivity:
-                        //Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(Timetable.this, "Favorites", Toast.LENGTH_SHORT).show();
+                        Intent MainActivity = new Intent(Timetable.this, MainActivity.class);
+                        startActivity(MainActivity);
+                        overridePendingTransition(0, 0);
                         break;
                     case R.id.action_Vvz:
-                        //Toast.makeText(MainActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
-                        Intent Vvz = new Intent(MainActivity.this, Vvz.class);
+                        //Toast.makeText(Timetable.this, "Nearby", Toast.LENGTH_SHORT).show();
+                        Intent Vvz = new Intent(Timetable.this, Vvz.class);
                         startActivity(Vvz);
                         overridePendingTransition(0, 0);
                         break;
@@ -50,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
 
 
 
--- app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Vvz.java
+++ app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Vvz.java
@@ -10,47 +10,40 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Vvz extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_vvz);
 
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.action_MainActivity);
+        bottomNavigationView.setSelectedItemId(R.id.action_Vvz);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_Timetable:
-                        //Toast.makeText(MainActivity.this, "Recents", Toast.LENGTH_SHORT).show();
-                        Intent Timetable = new Intent(MainActivity.this,Timetable.class);
+                        //Toast.makeText(Vvz.this, "Recents", Toast.LENGTH_SHORT).show();
+                        Intent Timetable = new Intent(Vvz.this, Timetable.class);
                         startActivity(Timetable);
-
-                        //This line makes a smooth transition and is used for all transitions in all the activities
                         overridePendingTransition(0, 0);
-
                         break;
                     case R.id.action_MainActivity:
-                        //Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(Vvz.this, "Favorites", Toast.LENGTH_SHORT).show();
+                        Intent MainActivity = new Intent(Vvz.this, MainActivity.class);
+                        startActivity(MainActivity);
+                        overridePendingTransition(0, 0);
                         break;
                     case R.id.action_Vvz:
-                        //Toast.makeText(MainActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
-                        Intent Vvz = new Intent(MainActivity.this, Vvz.class);
-                        startActivity(Vvz);
-                        overridePendingTransition(0, 0);
+                        //Toast.makeText(Vvz.this, "Nearby", Toast.LENGTH_SHORT).show();
                         break;
                 }
                 return true;
             }
         });
-
 
 
     }
