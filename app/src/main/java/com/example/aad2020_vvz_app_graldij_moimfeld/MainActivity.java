53831ba5a476212f7df96e265f156f53c5498370
moimfeld <moritz.imfeld@gmail.com>
2020-11-05 08:48:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.aad2020_vvz_app_graldij_moimfeld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
