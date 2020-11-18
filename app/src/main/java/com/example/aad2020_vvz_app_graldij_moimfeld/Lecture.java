a937baaa02e587eebae659b1f0b6a6f832a70118
graldij <graldij@student.ethz.ch>
2020-11-18 11:52:11 +0100

    Task accomplished: successfully parsed a name of the lecture and displayed as a Toast. Needed to do that: Parse class which runs the JSoup-->Document as a background thread.

--- app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Lecture.java
+++ app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Lecture.java
@@ -1,12 +1,13 @@
 package com.example.aad2020_vvz_app_graldij_moimfeld;
+import java.io.*;
 
 public class Lecture {
-    String name;
-    String day;
-    String start_time;
-    String end_time;
-    String lecture_code;
-    int ECTS;
+    public String name;
+    public String day;
+    public String start_time;
+    public String end_time;
+    public String lecture_code;
+    public int ECTS;
 
     public Lecture(String name, String day, String start_time, String end_time, String lecture_code, int ECTS) {
         this.name = name;
--- app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/MainActivity.java
+++ app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/MainActivity.java
@@ -4,14 +4,31 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+
+public class MainActivity extends AppCompatActivity {
+//    Parse new_parse = new Parse();
+//    Document doc = new_parse.doInBackground("http://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lang=en&semkez=2020W&ansicht=ALLE&lerneinheitId=140984&");
+
+    public Lecture parse(Document doc){
+        Lecture result;
+        result= new Lecture("ciao_name", "ciao_day", "ciao_start", "ciao_end", "ciao_code", 14);
+
+        return result;
+    }
 
 
     @Override
@@ -20,7 +37,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+         AsyncTask<String, Void, Document> task = new Parse().execute("http://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lang=en&semkez=2020W&ansicht=ALLE&lerneinheitId=140984&");
+        try {
+            Document doc = task.get();
+            Element content = doc.getElementById("contentTop");
+            Toast.makeText(MainActivity.this, content.text(), Toast.LENGTH_SHORT).show();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+
+
+
+
+//        Document doc = new_parse.doInBackground("http://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lang=en&semkez=2020W&ansicht=ALLE&lerneinheitId=140984&");
+
+
+//        Toast.makeText(MainActivity.this, parsed_lecture.name, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(MainActivity.this, parsed_lecture.start_time, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(MainActivity.this, parsed_lecture.end_time, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(MainActivity.this, String.valueOf(parsed_lecture.ECTS), Toast.LENGTH_SHORT).show();
+//        Toast.makeText(MainActivity.this, parsed_lecture.day, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(MainActivity.this, parsed_lecture.lecture_code, Toast.LENGTH_SHORT).show();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.action_MainActivity);
@@ -52,5 +92,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    Lecture sample_lecture = new Lecture();
+
 }
+
+
+
