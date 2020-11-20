97771a33ecae6ac566f7bd1458eb8fa9eee023ab
moimfeld <moritz.imfeld@gmail.com>
2020-11-20 15:28:56 +0100

    renamed Parse class to NewThread class, so we can use Parse class for the real parsing

---
+++ app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/NewThread.java
@@ -0,0 +1,27 @@
+package com.example.aad2020_vvz_app_graldij_moimfeld;
+
+import android.os.AsyncTask;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class NewThread extends AsyncTask<String, Void, Document> {
+
+
+    @Override
+    public Document doInBackground(String... params) {
+
+        Document doc;
+        try {
+
+            doc = Jsoup.connect(params[0]).get();
+            return doc;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
