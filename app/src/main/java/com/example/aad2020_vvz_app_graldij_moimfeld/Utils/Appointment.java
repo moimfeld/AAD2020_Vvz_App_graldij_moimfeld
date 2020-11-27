a9cdf1a719ed5e3df210d7fae7d7cb311d9fa396
moimfeld <moritz.imfeld@gmail.com>
2020-11-27 16:03:15 +0100

    added a selected variable to the appointment class

--- app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Utils/Appointment.java
+++ app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Utils/Appointment.java
@@ -8,13 +8,15 @@ public class Appointment {
     public ArrayList<Integer> time;
     public String periodicity;
     public ArrayList<String> dates;
+    public String place;
     public boolean selected;
 
-    public Appointment(String day, ArrayList<Integer> time, String periodicity, ArrayList<String> dates){
+    public Appointment(String day, ArrayList<Integer> time, String periodicity, ArrayList<String> dates, String place){
         this.day = day;
         this.time = time;
         this.periodicity = periodicity;
         this.dates = dates;
+        this.place = place;
         this.selected = true;
     }
 
