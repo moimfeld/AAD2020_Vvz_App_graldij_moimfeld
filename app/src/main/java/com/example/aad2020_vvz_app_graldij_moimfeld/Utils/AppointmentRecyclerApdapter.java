98bc3eb61f32374c8ecc5c5c9d56649efb078b0c
moimfeld <moritz.imfeld@gmail.com>
2020-12-02 19:03:14 +0100

    added the implementations of the switches in the popup

--- app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Utils/AppointmentRecyclerApdapter.java
+++ app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Utils/AppointmentRecyclerApdapter.java
@@ -4,10 +4,14 @@ import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.aad2020_vvz_app_graldij_moimfeld.Activities.MainActivity;
 import com.example.aad2020_vvz_app_graldij_moimfeld.R;
 import java.util.ArrayList;
 
@@ -32,12 +36,23 @@ public class AppointmentRecyclerApdapter extends RecyclerView.Adapter<Appointmen
 
     @SuppressLint("SetTextI18n")
     @Override
-    public void onBindViewHolder(@NonNull AppointmentRecyclerApdapter.AppointmentItemHolder holder, int position) {
+    public void onBindViewHolder(@NonNull AppointmentRecyclerApdapter.AppointmentItemHolder holder, final int position) {
         // We set the texts and the image of our MenuItemHolder object
         holder.category.setText(appointments.get(position).category);
         holder.day.setText("Day: " + appointments.get(position).day);
         holder.time.setText("Time: " + Integer.toString(appointments.get(position).getStartTime()) + " - " + Integer.toString(appointments.get(position).getEndTime()));
         holder.place.setText("Place: " + appointments.get(position).place);
+        holder.aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    appointments.get(position).selected = true;
+                } else {
+                    appointments.get(position).selected = false;
+                }
+            }
+        });
+        //important that this next line gets executed after the holder.aSwitch.setOnCheckedChangeListener.... or else the switched dont behave, and sometimes toggle back to the on state when scrolling fast
+        holder.aSwitch.setChecked(appointments.get(position).selected);
     }
 
     @Override
@@ -48,6 +63,7 @@ public class AppointmentRecyclerApdapter extends RecyclerView.Adapter<Appointmen
 
     static class AppointmentItemHolder extends RecyclerView.ViewHolder {
         TextView category, day, time, place;
+        Switch aSwitch;
         LinearLayout linearLayoutAppointment;
 
         public AppointmentItemHolder(@NonNull View itemView) {
@@ -57,6 +73,7 @@ public class AppointmentRecyclerApdapter extends RecyclerView.Adapter<Appointmen
             day = itemView.findViewById(R.id.recycler_row_appointment_day);
             time = itemView.findViewById(R.id.recycler_row_appointment_time);
             place = itemView.findViewById(R.id.recycler_row_appointments_place);
+            aSwitch = itemView.findViewById(R.id.switch1);
             linearLayoutAppointment = itemView.findViewById(R.id.recycler_row_appointment_linearlayout);
 
 
