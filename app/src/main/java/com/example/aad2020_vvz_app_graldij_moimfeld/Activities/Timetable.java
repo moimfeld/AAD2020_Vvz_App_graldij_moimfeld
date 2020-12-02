2a96abef5ff8d9bc97677f1ec235feaccfd055f4
graldij <graldij@student.ethz.ch>
2020-12-02 12:17:56 +0100

    New Timetable layout and  activity to add events to the Table.

--- app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Activities/Timetable.java
+++ app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Activities/Timetable.java
@@ -1,25 +1,58 @@
 package com.example.aad2020_vvz_app_graldij_moimfeld.Activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.alamkanak.weekview.WeekView;
 import com.example.aad2020_vvz_app_graldij_moimfeld.R;
+import com.example.aad2020_vvz_app_graldij_moimfeld.Utils.Appointment;
+import com.example.aad2020_vvz_app_graldij_moimfeld.Utils.Course;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class Timetable extends AppCompatActivity {
 
 
-
-
-
+    ArrayList<String> used_ids = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
+
+
+        for(Course course : MainActivity.saved_courses){
+            String name=course.name;
+            for (Appointment appointment : course.getAllAppointments()){
+//                random color
+                String day= appointment.day;
+
+                //choose only if the course_unit is selected
+                for (int time: appointment.time){
+                    String cell = day+"_"+time;
+                    if(!used_ids.contains(cell)){
+                        used_ids.add(cell);
+                        cell="Mon_8";
+                        int id = getResources().getIdentifier(cell, "id", getPackageName());
+                        TextView text = findViewById(id);
+                        text.setText(name);
+                        text.setHeight(800);
+                        //set background with random color, mixed if more lectures
+                    }
+
+
+                }
+            }
+
+        }
+
 
 
 
