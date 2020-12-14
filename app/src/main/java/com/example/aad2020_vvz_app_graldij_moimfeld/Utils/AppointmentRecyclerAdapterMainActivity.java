0f3b312888cb95f3afb86eb8f275bd40e3165bb9
moimfeld <moritz.imfeld@gmail.com>
2020-12-14 13:58:30 +0100

    fixed some non-uniform naming

--- app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Utils/AppointmentRecyclerAdapterMainActivity.java
+++ app/src/main/java/com/example/aad2020_vvz_app_graldij_moimfeld/Utils/AppointmentRecyclerAdapterMainActivity.java
@@ -75,14 +75,14 @@ public class AppointmentRecyclerAdapterMainActivity extends RecyclerView.Adapter
                         button.getVisibility();
                         button.setVisibility(View.GONE);
                         button.setWidth(0);
-                        collision.setText("no collisions found");
+                        collision.setText("no conflicts found");
                         collision.setWidth(100);
                         collision.setTextColor(Color.parseColor("#4CAF50"));
                     }
 
                     else {
                         button.setVisibility(View.VISIBLE);
-                        collision.setText(Integer.toString(MainActivity.collisions.size()) + " collisions found");
+                        collision.setText(Integer.toString(MainActivity.collisions.size()) + " conflicts found");
                         collision.setTextColor(Color.parseColor("#FF0000"));
                     }
                 } else {
